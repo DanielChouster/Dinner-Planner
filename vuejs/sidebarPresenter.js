ae0bfa45b2ceb67386b9e44ef85e9960c119eb9b
Daniel Chouster <chouster@kth.se>
2021-11-15 12:01:38 +0100

    t.o.m. slide 45

--- vuejs/sidebarPresenter.js
+++ vuejs/sidebarPresenter.js
@@ -8,17 +8,3 @@ function SidebarPresenter(props) {   // assume a model prop
         removeDish={param => { return props.model.removeFromMenu(param);}}
         />;
 }
-function SidebarPresenter(props) {   // assume a model prop
-    return <SidebarView guests={props.model.numberOfGuests}
-        setGuests={param => { return props.model.setNumberOfGuests(param); }
-                          /* TODO custom event callback 
-           that calls the method setNumberOfGuests of props.model  */}
-        dishes={props.model.dishes}
-        dishChoice={param => {props.model.setCurrentDish(param)}} 
-        removeDish={param => {props.model.removeFromMenu(param)}}
-    />;
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> ad6eab72913e20e1dd17957ba8372141aeb8c370
