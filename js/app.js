52de9a6864112358530de7e78ce60608c66ee2f6
Ville Vik <villevik18@gmail.com>
2021-11-15 16:46:00 +0100

    tw2.5

--- js/app.js
+++ js/app.js
@@ -12,9 +12,9 @@
 function RenderTest() { console.log("Vue sub-component render test"); return false; }
 function App(props) {
     return (
-        <div>
-            <SidebarPresenter model={props.model} />
-            <SummaryPresenter model={props.model} />
+        <div class="flexParent">
+            <div class="sidebar debug"><SidebarPresenter model={props.model} /></div>
+            <div class="maincontent debug"><SummaryPresenter model={props.model} /></div>
             <RenderTest />
         </div>
     );
